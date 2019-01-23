cf93ff8058fa2f4c71e1109efd5be1efdff62d70
Melissa Bain <apps@melissadbain.com>
2019-01-23 11:38:09 -0600

    Successful Springboot Installation with Hello World!

---
+++ src/main/java/com/melissadbain/wildlife/WildlifeApplication.java
@@ -0,0 +1,14 @@
+package com.melissadbain.wildlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildlifeApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/melissadbain/wildlife/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.melissadbain.wildlife.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index() {
+
+        return "Hello World!";
+    }
+}
