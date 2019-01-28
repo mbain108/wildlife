996124405de9495abec3ba336556f173436d8a33
Melissa Bain <apps@melissadbain.com>
2019-01-28 13:02:26 -0600

    Initial pages with bare minimum functionality (View, Edit, Add)

--- src/main/java/com/melissadbain/wildlife/controllers/PatientController.java
+++ src/main/java/com/melissadbain/wildlife/controllers/PatientController.java
@@ -4,7 +4,11 @@ import com.melissadbain.wildlife.models.data.PatientDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import com.melissadbain.wildlife.models.Patient;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("patient")
@@ -16,9 +20,77 @@ public class PatientController {
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("patients", patientDao.findAll());
         model.addAttribute("title", "List All Patients");
+        model.addAttribute("patients", patientDao.findAll());
 
         return "patient/index";
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.GET)
+    public String displayCreatePatientForm(Model model) {
+
+        model.addAttribute("title", "New Patient");
+        model.addAttribute(new Patient());
+
+        return "patient/create";
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public String processCreatePatientForm(@ModelAttribute @Valid Patient newPatient,
+                                           Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+
+            model.addAttribute("title", "New Patient");
+
+            return "patient/create";
+        }
+
+        patientDao.save(newPatient);
+
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "view/{patientId}", method = RequestMethod.GET)
+    public String viewPatient(Model model, @PathVariable int patientId) {
+
+        Patient patient = patientDao.findOne(patientId);
+
+        model.addAttribute("patient", patient);
+        model.addAttribute("patientId", patient.getId());
+
+        return "patient/view";
+    }
+
+    @RequestMapping(value="edit/{patientId}", method = RequestMethod.GET)
+    public String displayEditPatientForm(Model model, @PathVariable int patientId) {
+
+        Patient editedPatient = patientDao.findOne(patientId);
+
+        model.addAttribute("title", "Edit - " + editedPatient.getId());
+        model.addAttribute("patient", editedPatient);
+
+        return "patient/edit";
+
+    }
+
+    @RequestMapping(value="edit/{patientId}", method = RequestMethod.POST)
+    public String processEditPatientForm(Model model, @ModelAttribute @Valid Patient patient,
+                                         @PathVariable int patientId, Errors errors) {
+
+        Patient editedPatient = patientDao.findOne(patientId);
+
+        if(errors.hasErrors()) {
+
+            model.addAttribute("title", "Edit - " + editedPatient.getId());
+
+            return "patient/edit";
+        }
+
+        editedPatient.setSpecies_name(patient.getSpecies_name());
+
+        patientDao.save(editedPatient);
+
+        return "redirect:/patient";
     }
 }
--- src/main/java/com/melissadbain/wildlife/models/Patient.java
+++ src/main/java/com/melissadbain/wildlife/models/Patient.java
@@ -11,30 +11,85 @@ public class Patient {
     @GeneratedValue
     private int id;
 
-    private String admitted_date;
+    @GeneratedValue
+    private String date_admitted;
 
-    @NotNull
-    @Size(min = 3, max = 100)
+    private Integer reference_number;
+    private Integer microchip_number;
     private String species_name;
-
-    @NotNull
-    @Size(min = 3, max = 1000)
+    private Integer number_of_patients;
+    private Integer rescuer_id;
+    private String rescuer_notes;
+    private Integer donation_id;
+    private String admitted_by;
+    private String transported_by;
+    private String address_found;
+    private String city_found;
+    private String state_found;
+    private String county_found;
+    private String date_found;
     private String reason;
+    private String care_from_user;
+    private String notes_about_rescue;
+    private String location;
+    private String band;
+    private String disposition;
+    private String case_year;
 
     public Patient() {}
 
-    public Patient(String species_name, String reason) {
+    public Patient(Integer reference_number, Integer microchip_number,
+                   String species_name, Integer number_of_patients, Integer rescuer_id, String rescuer_notes, Integer donation_id,
+                   String admitted_by, String transported_by, String address_found, String city_found,
+                   String state_found, String county_found, String date_found, String reason, String care_from_user,
+                   String notes_about_rescue, String location, String band, String disposition,
+                   String case_year) {
 
+        this.reference_number = reference_number;
+        this.microchip_number = microchip_number;
         this.species_name = species_name;
+        this.number_of_patients = number_of_patients;
+        this.rescuer_id = rescuer_id;
+        this.rescuer_notes = rescuer_notes;
+        this.donation_id = donation_id;
+        this.admitted_by = admitted_by;
+        this.transported_by = transported_by;
+        this.address_found = address_found;
+        this.city_found = city_found;
+        this.state_found = state_found;
+        this.county_found = county_found;
+        this.date_found = date_found;
         this.reason = reason;
+        this.care_from_user = care_from_user;
+        this.notes_about_rescue = notes_about_rescue;
+        this.location = location;
+        this.band = band;
+        this.disposition = disposition;
+        this.case_year = case_year;
     }
 
     public int getId() {
         return id;
     }
 
-    public String getAdmitted_date() {
-        return admitted_date;
+    public String getDate_admitted() {
+        return date_admitted;
+    }
+
+    public Integer getReference_number() {
+        return reference_number;
+    }
+
+    public void setReference_number(Integer reference_number) {
+        this.reference_number = reference_number;
+    }
+
+    public Integer getMicrochip_number() {
+        return microchip_number;
+    }
+
+    public void setMicrochip_number(Integer microchip_number) {
+        this.microchip_number = microchip_number;
     }
 
     public String getSpecies_name() {
@@ -45,11 +100,147 @@ public class Patient {
         this.species_name = species_name;
     }
 
+    public Integer getNumber_of_patients() {
+        return number_of_patients;
+    }
+
+    public void setNumber_of_patients(Integer number_of_patients) {
+        this.number_of_patients = number_of_patients;
+    }
+
+    public Integer getRescuer_id() {
+        return rescuer_id;
+    }
+
+    public void setRescuer_id(Integer rescuer_id) {
+        this.rescuer_id = rescuer_id;
+    }
+
+    public String getRescuer_notes() {
+        return rescuer_notes;
+    }
+
+    public void setRescuer_notes(String rescuer_notes) {
+        this.rescuer_notes = rescuer_notes;
+    }
+
+    public Integer getDonation_id() {
+        return donation_id;
+    }
+
+    public void setDonation_id(Integer donation_id) {
+        this.donation_id = donation_id;
+    }
+
+    public String getAdmitted_by() {
+        return admitted_by;
+    }
+
+    public void setAdmitted_by(String admitted_by) {
+        this.admitted_by = admitted_by;
+    }
+
+    public String getTransported_by() {
+        return transported_by;
+    }
+
+    public void setTransported_by(String transported_by) {
+        this.transported_by = transported_by;
+    }
+
+    public String getAddress_found() {
+        return address_found;
+    }
+
+    public void setAddress_found(String address_found) {
+        this.address_found = address_found;
+    }
+
+    public String getCity_found() {
+        return city_found;
+    }
+
+    public void setCity_found(String city_found) {
+        this.city_found = city_found;
+    }
+
+    public String getState_found() {
+        return state_found;
+    }
+
+    public void setState_found(String state_found) {
+        this.state_found = state_found;
+    }
+
+    public String getCounty_found() {
+        return county_found;
+    }
+
+    public void setCounty_found(String county_found) {
+        this.county_found = county_found;
+    }
+
+    public String getDate_found() {
+        return date_found;
+    }
+
+    public void setDate_found(String date_found) {
+        this.date_found = date_found;
+    }
+
     public String getReason() {
         return reason;
     }
 
     public void setReason(String reason) {
         this.reason = reason;
+    }
+
+    public String getCare_from_user() {
+        return care_from_user;
+    }
+
+    public void setCare_from_user(String care_from_user) {
+        this.care_from_user = care_from_user;
+    }
+
+    public String getNotes_about_rescue() {
+        return notes_about_rescue;
+    }
+
+    public void setNotes_about_rescue(String notes_about_rescue) {
+        this.notes_about_rescue = notes_about_rescue;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
+
+    public String getDisposition() {
+        return disposition;
+    }
+
+    public void setDisposition(String disposition) {
+        this.disposition = disposition;
+    }
+
+    public String getCase_year() {
+        return case_year;
+    }
+
+    public void setCase_year(String case_year) {
+        this.case_year = case_year;
     }
 }
